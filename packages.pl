9b1b0300e46396f1e20a974758a202a8464b9bd3
Mateu X Hunter <hunter@missoula.org>
2014-12-02 13:48:52 -0700

    begin package list

---
+++ packages.pl
@@ -0,0 +1,13 @@
+use Rex::Commands::Pkg;
+
+install package => [
+  "build-essential",
+  "curl",
+  "libexpat1-dev",
+  "libnet-ssh2-perl",
+  "tmux",
+  "irssi",
+  "git",
+  "keychain",
+  "emacs24-nox",
+];
